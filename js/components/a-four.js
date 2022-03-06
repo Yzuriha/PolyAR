6cc7712f5805c5a447aa78c71b6c7a9c86e872eb
Yzuriha <inoriislove@gmail.com>
2022-03-06 23:00:06 +0100

    Add Cuboids Marker

--- js/components/a-four.js
+++ js/components/a-four.js
@@ -13,11 +13,7 @@ AFRAME.registerComponent('a-four', {
         const square = new THREE.Shape();
         let rotationAxis = data.rotationAxis;
 
-        // console.log("doc", document.body.getBoundingClientRect());
-        // console.log("doc2", window.innerHeight, window.innerWidth);
-        // console.log("doc3", window.height, window.width);
         square.moveTo(0, 0);
-        // rotationAxis === "bottom" || left
         square.lineTo(data.width, 0);
         square.lineTo(data.width, data.height);
         square.lineTo(0, data.height);
