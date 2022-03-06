a6f2ac4b5d4fb8a5bf120857fbd7249c9791693d
Yzuriha <inoriislove@gmail.com>
2022-03-06 14:04:44 +0100

    Fix Embedded Raycaster Bug #2

--- js/components/custom-cursor.js
+++ js/components/custom-cursor.js
@@ -1,8 +1,7 @@
 AFRAME.registerComponent('custom-cursor', {
     init: function () {
 
-        this.el.addEventListener("cameraready", () => {
-            console.log("cameraready");
+        setTimeout(() => {
             const scene = AFRAME.scenes[0];
 
             const mouseCursor = document.createElement('a-entity');
@@ -10,18 +9,7 @@ AFRAME.registerComponent('custom-cursor', {
             mouseCursor.setAttribute('raycaster', 'objects', '.clickable');
             scene.appendChild(mouseCursor);
             console.log("new cursr");
-        });
-
-
-        // setTimeout(() => {
-        //     const scene = AFRAME.scenes[0];
-        //
-        //     const mouseCursor = document.createElement('a-entity');
-        //     mouseCursor.setAttribute('cursor', 'rayOrigin', 'mouse');
-        //     mouseCursor.setAttribute('raycaster', 'objects', '.clickable');
-        //     scene.appendChild(mouseCursor);
-        //     console.log("new cursr");
-        // }, 2000);
+        }, 2000);
     },
 
 });
