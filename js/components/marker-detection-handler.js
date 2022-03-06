02119e6a0853cd95e5119648481591cc2a0755ff
Yzuriha <inoriislove@gmail.com>
2022-03-06 15:12:38 +0100

    Update CSS + Add Object Name Recognition

--- js/components/marker-detection-handler.js
+++ js/components/marker-detection-handler.js
@@ -1,11 +1,14 @@
 AFRAME.registerComponent("marker-detection-handler", {
     init: function () {
+        let informationText = document.querySelector(".instruction__text")
         this.el.addEventListener("markerFound", (e) => {
             console.log("MARKER FOUND");
+            document.querySelector(".instruction__text").innerText = this.el.getAttribute("data-object-name")
         });
 
         this.el.addEventListener("markerLost", (e) => {
             console.log("MARKER LOST");
+            document.querySelector(".instruction__text").innerText = ""
         });
     }
 });
